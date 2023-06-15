e23260872e0a59493b70181343f1abe42505db27
Daniel Martínez <dagmtzs@gmail.com>
2023-06-14 20:52:07 -0600

    Update main.ino
    Back to main functionality, now branching to add new functions one by one

--- main/main.ino
+++ main/main.ino
@@ -15,8 +15,6 @@
 #define HOURS_PER_DAY 24
 #define SECOND_CHANGE_THRESHOLD 200
 
-const int led_pin = 13;
-
 unsigned int hours = 0;
 unsigned int minutes = 0;
 unsigned int seconds = 0;
@@ -30,66 +28,56 @@ void setup()
 {
   Serial.begin(9600);
   delay(SECOND_CHANGE_THRESHOLD);
-  DDRB |= B00111111;
-}
-
-// Only for PORTB
-void outputBCD( uint8_t number )
-{
-  uint8_t portbBuffer = PORTB;
-  
-  portbBuffer &= B00100000;
-  number &= B00001111;
-  portbBuffer |= number;
-
-  PORTB = portbBuffer;
 }
 
 void updateBuffer()
 {
+  msCounter = millis() % 1000;
+
   if ( msCounter <= SECOND_CHANGE_THRESHOLD && !newSecondFlag )
   {
     seconds++;
+    if ( seconds == SECONDS_PER_MINUTE )
+    {
+      seconds = 0;
+      minutes++;
+    }
+
+    if ( minutes == MINUTES_PER_HOUR )
+    {
+      minutes = 0;
+      hours++;
+    }
+
+    if ( hours == HOURS_PER_DAY )
+    {
+      hours = 0;
+    }
+
     newSecondFlag = true;
-    PORTB |= B00100000;
   }
 
   if ( msCounter > SECOND_CHANGE_THRESHOLD && newSecondFlag )
   {
     newSecondFlag = false;
-    PORTB &= B11011111;
-  }
-
-  if ( seconds == SECONDS_PER_MINUTE )
-  {
-    seconds = 0;
-    minutes++;
-  }
-
-  if ( minutes == MINUTES_PER_HOUR )
-  {
-    minutes = 0;
-    hours++;
-  }
-
-  if ( hours == HOURS_PER_DAY )
-  {
-    hours = 0;
   }
   
   sprintf(timeBuffer, "%02d:%02d:%02d", hours, minutes, seconds);
 }
 
-void loop() 
+void printTime()
 {
-  msCounter = millis() % 1000;
-  updateBuffer();
-
   if ( time[7] != timeBuffer[7] )
   {
     strcpy(time, timeBuffer);
     Serial.println(timeBuffer);
-    outputBCD(time[7]);
   }
+}
+
+void loop() 
+{
+
+  updateBuffer();
+  printTime();
   
 }
